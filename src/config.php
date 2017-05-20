cfae4574d8211d1cb71271b3e1f5dbbe3fb4ae02
root <yanpeng_1991@126.com>
2017-05-20 14:41:25 +0800

    ´´½¨ÏîĿ

---
+++ src/config.php
@@ -0,0 +1,15 @@
+<?php
+//mysql database address
+define('DB_HOST','127.0.0.1');
+//mysql database user
+define('DB_USER','admin');
+//database password
+define('DB_PASSWD','');
+//database name
+define('DB_NAME','test');
+//database prefix
+define('DB_PREFIX','tb_');
+//auth key
+define('AUTH_KEY','!hM%tBLyMayxztVRvlgr&dyHS*VcQu7Z1112e7da2c1e2ca5eb7e4651881e05db');
+//cookie name
+define('AUTH_COOKIE_NAME','EM_AUTHCOOKIE_Ux6sckBfAJpjdSH1MOK9ebV8YkDEczD1');
